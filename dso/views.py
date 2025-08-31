4b43da727a4d78489110e6f65e33cea820244e61
adityajha240220 <adityajha240220@gmail.com>
2025-08-31 12:00:52 +0530

    hlo

--- dso/views.py
+++ dso/views.py
@@ -4,8 +4,6 @@ from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth import logout
 
-# Create your views here.
-
 # Write a function-based view in Django that returns “Hello, Django” as an HTTP response.
 
 def hello_view(request):
